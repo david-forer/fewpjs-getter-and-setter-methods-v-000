e90c236a3accfc6f18eefad6e02e80c7cded1dc5
David Forer <david.j.forer@gmail.com>
2019-12-01 13:43:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 
 class Circle {
   constructor(radius) {
-    this.radius = radius
+    this.radius = radius;
   }
+  
+  get diameter() {
+    return this.radius * 2;
+  }
+  
+  get circumference() {
+    return Math.PI * this.diameter;
+  }
+  
+  get area() {
+    return Math.PI * (this.radius ** 2);
+  }
+  
+  
+  
 }
